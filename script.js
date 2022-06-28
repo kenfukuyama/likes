b8604dd4451ee29085fd9a0c60d56f0ae0e3913a
Ken Fukuyama <kenfukuyama0112@gmail.com>
2022-06-27 23:16:57 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+likeCount = 0;
+
+function addLike(element) {
+    // console.log(element);
+    // likeCount++;
+    // element.innerText = likeCount + " like(s)";
+    // element++;
+    element.innerText++;
+}
+
+
+function like(element) {
+    element.innerText++;
+}
+
+
+
